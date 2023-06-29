387dff2792e96419503cb549f81ba30d2f108de1
Alexis <alexis_keeter@yahoo.com>
2023-06-29 16:16:38 -0700

    create base of Contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact Me!</h1>
+            <ul>
+                <li>Email:</li>
+                <li>GitHub: </li>
+                <li>LinkedIn: </li>
+            </ul>
+        </div>
+    )
+}
