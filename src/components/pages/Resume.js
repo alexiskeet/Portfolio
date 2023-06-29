1e7126d3e0981b98647307615bb44ac1ef135662
Alexis <alexis_keeter@yahoo.com>
2023-06-29 16:31:25 -0700

    added outline to Resume page

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Resume() {
+    return (
+        <div>
+            <p>
+                Here's the link to my resume:
+            </p>
+        </div>
+    )
+}
