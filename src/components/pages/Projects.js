29a9c1ed029eb41fd26fb6b8e8f4aed8f4e23087
Alexis <alexis_keeter@yahoo.com>
2023-06-29 16:28:54 -0700

    created outline for Projects page

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Projects() {
+    return (
+        <div> 
+            <h1>Project</h1>
+            <ul>
+                <li>insert projects here</li>
+            </ul>
+        </div>
+    )
+}
