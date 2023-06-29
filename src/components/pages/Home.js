1aa2fe32f5e88513fc1613e873539733c99db16a
Alexis <alexis_keeter@yahoo.com>
2023-06-29 16:22:15 -0700

    created a Home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>
+                This is my Portfolio - a place to keep the projects I've worked on in the Coding Bootcamp and what I've worked on afterwards!
+            </p>
+        </div>
+    )
+}
