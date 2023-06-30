d82728cadb609777c825740463508062d72816ad
Alexis <alexis_keeter@yahoo.com>
2023-06-29 21:39:48 -0700

    added projects to Project page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,9 +4,68 @@ export default function Projects() {
     return (
         <div> 
             <h1>Project</h1>
-            <ul>
-                <li>insert projects here</li>
-            </ul>
+          <div>
+            <nav>
+                <a href='https://inkwatch.herokuapp.com/'>Ink Watch (deployed site)</a>
+                <a href=' https://github.com/wobbledy/ink-watch.git'>Ink Watch (repository)</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href='https://redux-shop-shop.herokuapp.com/'>Redux Shop Shop (deployed site)</a>
+                <a href='https://github.com/alexiskeet/Redux-Shop-Shop.git'>Redux Shop Shop (repository)</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href='https://sleepy-citadel-25242.herokuapp.com/Links to an external site.'>Book Search Engine (deployed site)</a>
+                <a href='https://github.com/alexiskeet/challenge-21.git'>Book Search Engine (repository)</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href='https://vast-shelf-40739.herokuapp.com/'>JATE (deployed site)</a>
+                <a href='https://github.com/alexiskeet/challenge-19.git'>JATE (repository)</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href='https://github.com/alexiskeet/NoSQL-Social-Network.git'>NoSQL Social Network (repository)</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href='https://github.com/alexiskeet/challenge-13.git'>E-Commerce Back End</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href='https://github.com/alexiskeet/Employee-Tracker.git'>Employee Tracker</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href='https://github.com/alexiskeet/challenge-9.git'>README Generator</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href='https://macaroniketchup.github.io/Webdev-Remote-Job-Search-App/'>Webdev Remote Job Search (deployed site)</a>
+                <a href='https://github.com/MacaroniKetchup/Webdev-Remote-Job-Search-App.git'>Webdev Remote Job Search (repository)</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href=' https://alexiskeet.github.io/Work-Day-Scheduler/'>Work Day Scheduler (deployed site)</a>
+                <a href='https://github.com/alexiskeet/Work-Day-Scheduler.git'>Work Day Scheduler (repository)</a>
+            </nav>
+          </div>
+          <div>
+            <nav>
+                <a href='https://alexiskeet.github.io/Coding-Quiz/Links to an external site.'>Coding Quiz (deployed site)</a>
+                <a href='https://github.com/alexiskeet/Coding-Quiz.git'>Coding Quiz (repository)</a>
+            </nav>
+          </div>
         </div>
     )
 }
